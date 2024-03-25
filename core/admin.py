94af8207159f446702f8f5a577ef91a13d55292b
PedroHenriqueDevBR <p.lyma18@gmail.com>
2024-03-24 23:03:23 -0300

    feat: models implementados

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Notificacao, StatusNotificacao
 
-# Register your models here.
+
+@admin.register(Notificacao)
+class NotificacaoAdmin(admin.ModelAdmin):
+    list_display = (
+        "destinatarios",
+        "assunto",
+        "conteudo",
+        "eh_html",
+        "sistema",
+    )
+
+
+@admin.register(StatusNotificacao)
+class StatusNotificacaoAdmin(admin.ModelAdmin):
+    list_display = (
+        "registrado_em",
+        "status",
+        "notificacao",
+    )
