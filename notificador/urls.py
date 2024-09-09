d19fe07f2268ef38f1c597fd8463f35ec0450d74
PedroHenriqueDevBR <p.lyma18@gmail.com>
2024-09-09 10:43:53 -0300

    fix: correcao no redirecionamento de detalhes de url

--- notificador/urls.py
+++ notificador/urls.py
@@ -6,6 +6,6 @@ from core.views import ApresentarNotificacaoView, IndexView, NotificarApiView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", IndexView.as_view(), name="index"),
-    path("/<int:pk>", ApresentarNotificacaoView.as_view(), name="apresentar"),
+    path("<int:pk>", ApresentarNotificacaoView.as_view(), name="apresentar"),
     path("api/v1/notificar", NotificarApiView.as_view(), name="notificar"),
 ]
