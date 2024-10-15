a278c819867615f54227223cd5b835a0d7e0dfcc
PedroHenriqueDevBR <p.lyma18@gmail.com>
2024-10-15 11:47:02 -0300

    feat: implementado filtro de por status das notificações

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from django.http.request import HttpRequest
 from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.core.paginator import Paginator
+from django.db.models import Q
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -16,21 +17,31 @@ from core.services.mail_service import MailService
 class IndexView(LoginRequiredMixin, View):
     permission_classes = [IsAuthenticated]
 
-    def buscar_notificacoes_sistemas(self, selecionados, pagina):
+    def buscar_notificacoes_sistemas(self, selecionados, pagina, status):
         objetos = []
         if len(selecionados) == 0:
             objetos = Notificacao.objects.all().order_by("-id")
+        else:
+            sistemas = []
+            for pk in selecionados:
+                sistemas_query = User.objects.filter(pk=pk)
+                if sistemas_query.exists():
+                    sistemas.append(sistemas_query[0])
 
-        sistemas = []
-        for pk in selecionados:
-            sistemas_query = User.objects.filter(pk=pk)
-            if sistemas_query.exists():
-                sistemas.append(sistemas_query[0])
+            objetos = Notificacao.objects.filter(
+                sistema__in=sistemas,
+            ).order_by("-id")
 
-        if len(sistemas) > 0:
-            objetos = Notificacao.objects.filter(sistema__in=sistemas)
+            if status != "" and status != "0":
+                status = int(status)
+                objetos = objetos.filter(status__status=status)
 
-        paginator = Paginator(objetos, 20)
+            if status == 1:
+                objetos = objetos.exclude(
+                    Q(status__status=2) | Q(status__status=3),
+                )
+
+        paginator = Paginator(objetos, 2)
         return paginator.get_page(pagina)
 
     def formatar_sistemas_args(self, selecionados):
@@ -39,16 +50,20 @@ class IndexView(LoginRequiredMixin, View):
             argumento += f"&sistema={selecionado}"
         return argumento
 
+    def formatar_status_args(self, status):
+        return f"&status={status}" if status != "" else ""
+
     def get(self, request: HttpRequest):
         args = request.GET
         selecionados = args.getlist("sistema")
         pagina = request.GET.get("page")
+        status = request.GET.get("status", "")
         selecionados = list(map(lambda pk: int(pk), selecionados))
 
         template_name = "index.html"
         context = {}
         context["notificacoes"] = self.buscar_notificacoes_sistemas(
-            selecionados=selecionados, pagina=pagina
+            selecionados=selecionados, pagina=pagina, status=status
         )
         context["sistemas"] = User.objects.filter(
             is_staff=False,
@@ -56,6 +71,9 @@ class IndexView(LoginRequiredMixin, View):
         )
         context["sistemas_args"] = self.formatar_sistemas_args(
             selecionados=selecionados,
+        )
+        context["status_args"] = self.formatar_status_args(
+            status=status,
         )
         context["selecionados"] = selecionados
         return render(request, template_name, context)
