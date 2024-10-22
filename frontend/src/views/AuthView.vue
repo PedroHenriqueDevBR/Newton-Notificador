ca589b5a73485a3ca84473d545143d817918cc14
PedroHenriqueDevBR <p.lyma18@gmail.com>
2024-10-22 14:05:33 -0300

    style: implementada página de autenticacao

--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -1,7 +1,38 @@
 <script setup></script>
 
 <template>
-  <h1>Auth page</h1>
+  <main class="uk-flex uk-flex-center uk-flex-middle">
+    <div class="uk-card uk-card-default uk-background-muted uk-padding">
+      <form>
+        <h1>Autenticação</h1>
+        <hr class="uk-divider-small">
+        <div class="uk-margin">
+          <label for="username" class="uk-form-label">Nome de usuário</label>
+          <div class="uk-form-controls">
+            <input type="text" class="uk-input" id="username" name="username" placeholder="username">
+          </div>
+        </div>
+
+        <div class="uk-margin">
+          <label for="password" class="uk-form-label">Senha</label>
+          <div class="uk-form-controls">
+            <input type="text" class="uk-input" id="password" name="password" placeholder="********">
+          </div>
+        </div>
+
+        <button class="uk-button uk-button-primary">Entrar</button>
+      </form>
+    </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+main {
+  height: 100vh;
+  width: 100%;
+}
+
+form {
+  min-width: 400px;
+}
+</style>
