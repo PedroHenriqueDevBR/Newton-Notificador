421caf8b174120d727ccd3e36a181aa12641bfb0
PedroHenriqueDevBR <p.lyma18@gmail.com>
2024-12-05 10:55:14 -0300

    feat: implementado metodo para encerrar sessão

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { useCarregandoStore } from '@/stores/carregando';
 import { useNotificacaoStore } from '@/stores/NotificacaoStore';
+import { useAuthStore } from '@/stores/AuthStore';
+import { useRouter } from 'vue-router';
 
 const notificacaoStore = useNotificacaoStore()
 const carregandoStore = useCarregandoStore()
+const authStore = useAuthStore()
+const router = useRouter()
 
 async function carregarNotificacoes(status) {
   carregandoStore.alterarStatus(true)
@@ -18,6 +22,19 @@ async function carregarNotificacoes(status) {
     carregandoStore.alterarStatus(false)
   }
 }
+
+async function sair() {
+  try {
+      await authStore.encerrarSessao()
+      router.push('/auth')
+  } catch (erro) {
+      if (erro instanceof AuthException) router.push('/auth')
+      if (erro instanceof ServerException) alert('Sem conexão com o servidor!')
+  } finally {
+    carregandoStore.alterarStatus(false)
+  }
+}
+
 </script>
 
 <template>
@@ -36,7 +53,7 @@ async function carregarNotificacoes(status) {
         </div>
 
         <div class="uk-navbar-right">
-          <button class="uk-button uk-button-default on-primary-color-text">
+          <button class="uk-button uk-button-default on-primary-color-text" v-on:click="sair()">
             Sair
           </button>
         </div>
